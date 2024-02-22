9f28204fc490516217702c24d9eba5a004164bb3
Hector <hmreluz@gmail.com>
2024-02-22 16:09:03 -0500

    fh: 02-bases: callbacks

--- fh-node-zero-expert/02-bases/src/app.js
+++ fh-node-zero-expert/02-bases/src/app.js
@@ -1,3 +1,15 @@
-const templateExports = require('./js-foundation/01-template')
+// const templateExports = require('./js-foundation/01-template')
 
-require('./js-foundation/02-destructuring')
+const { getUserById } = require('./js-foundation/03-callbacks')
+
+// require('./js-foundation/02-destructuring')
+require('./js-foundation/03-callbacks')
+
+const id = 2
+getUserById(id, function(error, user){
+    if (error) {
+        throw new Error(error, id)
+    }
+
+    console.log(user)
+})
