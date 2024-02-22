4efc07287f65139212b20f640ca0ca055d978608
Hector <hmreluz@gmail.com>
2024-02-22 16:19:37 -0500

    fh: 02-bases: arrow functions

--- fh-node-zero-expert/02-bases/src/app.js
+++ fh-node-zero-expert/02-bases/src/app.js
@@ -3,10 +3,11 @@
 const { getUserById } = require('./js-foundation/03-callbacks')
 
 // require('./js-foundation/02-destructuring')
-require('./js-foundation/03-callbacks')
+// require('./js-foundation/03-callbacks')
+// require('./js-foundation/04-arrows')
 
 const id = 2
-getUserById(id, function(error, user){
+getUserById(id, (error, user) => {
     if (error) {
         throw new Error(error, id)
     }
