4a09ac50e8f60dc91997ff1b6e6e8f2e183a3591
Hector <hmreluz@gmail.com>
2024-02-22 19:48:06 -0500

    fh: 02-bases: async/await

--- fh-node-zero-expert/02-bases/src/js-foundation/06-promises.js
+++ fh-node-zero-expert/02-bases/src/js-foundation/06-promises.js
@@ -1,10 +1,23 @@
-const getPokemonById = (id, callback) => {
+// const getPokemonById = (id, callback) => {
+//     const url = `https://pokeapi.co/api/v2/pokemon/${id}`
+
+//     return fetch(url)
+//     .then((response) =>  response.json())
+//     // .then(() => {throw new Error('Pokemon does not exist')})
+//     .then((pokemon) => pokemon.name)
+// }
+
+const getPokemonById = async(id) => {
     const url = `https://pokeapi.co/api/v2/pokemon/${id}`
 
-    return fetch(url)
-    .then((response) =>  response.json())
-    // .then(() => {throw new Error('Pokemon does not exist')})
-    .then((pokemon) => pokemon.name)
+    const resp = await fetch(url);
+
+    const pokemon = await resp.json()
+
+    throw new Error('Pokemon does not exist!')
+
+    return pokemon.name
+
 }
 
 module.exports = getPokemonById
