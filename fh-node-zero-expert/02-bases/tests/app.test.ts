fe0472067dc0502f97712dcbc732b94fd36617a5
Hector <hmreluz@gmail.com>
2024-03-02 00:09:55 -0500

    04-introduccion-al-testing: arrange, act and assert

--- fh-node-zero-expert/02-bases/tests/app.test.ts
+++ fh-node-zero-expert/02-bases/tests/app.test.ts
@@ -1,6 +1,14 @@
 
-describe('App', () => {
-    test('should be true', () => {
-        expect(true).toBe(true)
+describe('Test in the App file', () => {
+
+    test('should be 30', () => {
+
+        const num1 = 10;
+        const num2 = 20;
+
+        const result = num1 + num2;
+
+        expect(result).toBe(30)
+
     })
 })
