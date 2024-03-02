116d073454979521ef88cf58d4400560cfc5173d
Hector <hmreluz@gmail.com>
2024-03-02 08:31:59 -0500

    04-introduccion-al-testing: test en 02-destructuring

--- fh-node-zero-expert/02-bases/src/js-foundation/02-destructuring.ts
+++ fh-node-zero-expert/02-bases/src/js-foundation/02-destructuring.ts
@@ -1,10 +1,10 @@
-console.log(process.env.PORT ?? 3000)
+// console.log(process.env.PORT ?? 3000)
 
 const {SHELL, HOMEBREW_PREFIX, npm_lifecycle_script} = process.env
 
-console.table({SHELL, HOMEBREW_PREFIX, npm_lifecycle_script})
+// console.table({SHELL, HOMEBREW_PREFIX, npm_lifecycle_script})
 
-const characters = ['Flash', 'Superman', 'Green Lantern', 'Batman']
+export const characters = ['Flash', 'Superman', 'Green Lantern', 'Batman']
 
 const [,, batman] = characters
-console.log(batman)
+// console.log(batman)
