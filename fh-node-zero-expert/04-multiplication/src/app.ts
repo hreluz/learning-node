6bb4375d15fce6f42e003d7999d25ffad6ba7af8
Hector <hmreluz@gmail.com>
2024-03-05 12:58:02 -0500

    05-clean-architecture-app-console: print in console and save in file

--- fh-node-zero-expert/04-multiplication/src/app.ts
+++ fh-node-zero-expert/04-multiplication/src/app.ts
@@ -1,3 +1,25 @@
-const message:string = "Hello world"
+const saveToFile = (data:string, number:number) => {
+    const fs = require('fs');
+    fs.mkdirSync('outputs', {recursive:true})
+    fs.writeFileSync(`outputs/tabla-${number}.txt`, data);
+    console.log('File created!')
+}
 
-console.log(message)
+const printAndSaveTable = (number:number, to:number) => {
+
+    let text = "================================\n"
+    text +=`\t\ Tabla del ${number}\n`
+    text +='================================\n'
+
+    for(let i = 0; i <= to; i++) {
+        text += `\n${number} x ${i} =  ${i*number}`
+    }
+
+    saveToFile(text, number)
+
+    console.log(text)
+}
+
+const number = 5
+
+printAndSaveTable(5, 10)
