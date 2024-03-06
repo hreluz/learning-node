a079edad3c2115ccfdb2e3f5dba7f0729af890ea
Hector <hmreluz@gmail.com>
2024-03-06 08:24:06 -0500

    05-clean-architecture-app-console: anonymous func and yarg options

--- fh-node-zero-expert/04-multiplication/src/app.ts
+++ fh-node-zero-expert/04-multiplication/src/app.ts
@@ -1,4 +1,14 @@
 import { yarg } from "./config/plugins/yargs.plugin";
 
 
-console.log(yarg)
+// console.log(yarg)
+
+(async() => {
+    await main()
+    console.log('End of program')
+})();
+
+
+async function main() {
+    console.log(yarg)
+}
--- fh-node-zero-expert/04-multiplication/src/config/plugins/yargs.plugin.ts
+++ fh-node-zero-expert/04-multiplication/src/config/plugins/yargs.plugin.ts
@@ -1,8 +1,25 @@
 import yargs from 'yargs'
 import {hideBin} from 'yargs/helpers'
 
-export const yarg = yargs(process.argv)
+export const yarg = yargs(hideBin(process.argv))
+    .option('b', {
+        alias: 'base',
+        type: 'number',
+        demandOption: true,
+        describe: 'Multiplicatio table base '
+    })
+    .option('l', {
+        alias: 'limit',
+        type: 'number',
+        default: 10,
+        describe: 'Multiplication table limit'
+    })
+    .option('s', {
+        alias: 'show',
+        type: 'boolean',
+        default: false,
+        describe: 'SHow multiplication table'
+    })
     .parseSync()
 
-    console.log(yarg)
 
