13f8871b12e0f6fe8702386136c5de50477c5f96
Hector <hmreluz@gmail.com>
2024-04-10 18:08:01 -0500

    06-testing: test on ServerApp -  test on App.ts

--- fh-node-zero-expert/04-multiplication/src/app.test.ts
+++ fh-node-zero-expert/04-multiplication/src/app.test.ts
@@ -1,5 +1,24 @@
+
+import { ServerApp } from './presentation/server-app';
+
 describe('App', () => {
-    test('should be true', () => {
-        expect(true).toBe(true);
+    
+    test('should call Serer.run with values', async() => {
+
+        const serverRunMock = jest.fn();
+
+        ServerApp.run = serverRunMock;
+
+        process.argv = ['node', 'app.ts', '-b', '10', '-l', '5', '-s', '-n', 'test-file', '-d', 'file-destination'];
+
+        await import('./app');
+
+        expect(serverRunMock).toHaveBeenCalledWith({
+            base: 10,
+            limit: 5,
+            showTable: true,
+            destination: 'file-destination',
+            name: 'test-file'
+        });
     });
 });
