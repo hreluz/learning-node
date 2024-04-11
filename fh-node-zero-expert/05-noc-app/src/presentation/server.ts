8f756313837208fb60858e3cd258b21643afa789
Hector <hmreluz@gmail.com>
2024-04-11 00:16:28 -0500

    07-noc-app: cron task

--- fh-node-zero-expert/05-noc-app/src/presentation/server.ts
+++ fh-node-zero-expert/05-noc-app/src/presentation/server.ts
@@ -1,7 +1,20 @@
+import {CronJob} from 'cron';
+
 export class Server {
 
     public static start() {
+        
         console.log('Server started');
+        
+        const job = new CronJob(
+            '*/2 * * * * *',
+            () => {
+                console.log('You will see this message every 2 seconds ', new Date());
+            }
+        )
+
+        job.start()
+
     }
 
 }
