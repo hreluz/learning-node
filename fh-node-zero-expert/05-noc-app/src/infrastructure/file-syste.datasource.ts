a6a7103b9134bf5469d46e5e7e0bfd6c89fb207f
Hector <hmreluz@gmail.com>
2024-04-21 12:54:57 -0500

    07-noc-app: clean-architecture: FileSystem - SaveLog

--- fh-node-zero-expert/05-noc-app/src/infrastructure/file-syste.datasource.ts
+++ fh-node-zero-expert/05-noc-app/src/infrastructure/file-syste.datasource.ts
@@ -1,11 +1,11 @@
-import { existsSync, mkdirSync, writeFileSync } from "fs";
+import { appendFileSync, existsSync, mkdirSync, writeFileSync } from "fs";
 import { LogDataSource } from "../domain/datasources/log.datasource";
 import { LogEntity, LogSeverityLevel } from "../domain/entities/log.entity";
 
 export class FileSystemDataSource implements LogDataSource{
 
     private readonly logPath = 'logs/';
-    private readonly allLogsPath = 'logs/logs-low.log';
+    private readonly allLogsPath = 'logs/logs-all.log';
     private readonly mediumLogsPath = 'logs/logs-medium.log';
     private readonly highLogsPath = 'logs/logs-high.log';
 
@@ -29,8 +29,19 @@ export class FileSystemDataSource implements LogDataSource{
         })
     }
 
-    saveLog(log: LogEntity): Promise<void> {
-        throw new Error("Method not implemented.");
+    async saveLog(newLog: LogEntity): Promise<void> {
+
+        const logAsJson = `${JSON.stringify(newLog)}\n`;
+
+        appendFileSync(this.allLogsPath, logAsJson);
+
+        if (newLog.level === LogSeverityLevel.low) return;
+
+        if (newLog.level === LogSeverityLevel.medium) {
+            appendFileSync(this.mediumLogsPath, logAsJson);
+        } else {
+            appendFileSync(this.highLogsPath, logAsJson);
+        }
     }
     getLogs(severityLevel: LogSeverityLevel): Promise<LogEntity[]> {
         throw new Error("Method not implemented.");
