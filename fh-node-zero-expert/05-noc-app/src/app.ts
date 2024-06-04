9651b498e2a5368e6f5202f44794190a3a21b2aa
Hector <hmreluz@gmail.com>
2024-06-03 22:39:00 -0500

    07-noc-app: databases (pg and mysql): Node + Mongo - Mongoose

--- fh-node-zero-expert/05-noc-app/src/app.ts
+++ fh-node-zero-expert/05-noc-app/src/app.ts
@@ -1,13 +1,21 @@
 import 'dotenv/config';
 
 import { Server } from "./presentation/server";
+import { MongoDatabase } from './data/mongo';
+import { envs } from './config/envs.plugin';
 // import { envs } from './config/envs.plugin';
 
 (async() => {
     await main();
 })();
 
-function main() {
+async function main() {
+
+    await MongoDatabase.connect({
+        mongoUrl: envs.MONGO_URL,
+        dbName: envs.MONGO_DB_NAME
+    })
+
     Server.start();
     // console.log(envs);
 }
